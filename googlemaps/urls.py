03e72d0e382a4f5b6a88df38183d1ca709996a0a
sudipbhandari126 <sudip.post@gmail.com>
2017-12-21 07:35:28 +0530

    small c hange

--- googlemaps/urls.py
+++ googlemaps/urls.py
@@ -13,13 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 from googlemaps import settings
 
 urlpatterns = [
-    path('maps/', include('maps.urls')),
-    path('admin/', admin.site.urls),
-] + static(settings.ICON_URL, document_root=settings.ICON_ROOT)
+                  path('maps/', include('maps.urls')),
+                  path('admin/', admin.site.urls),
+                  url(r'^$', RedirectView.as_view(url='https://about.me/sudip.post', permanent=False)),
+              ] + static(settings.ICON_URL, document_root=settings.ICON_ROOT)
